489e4b93eb863280675a09ef83ff1d27413c6ef8
Soliken <kennethrex456@gmail.com>
2024-06-25 20:06:37 +0800

    Skills Menu

--- src/app/skills/page.js
+++ src/app/skills/page.js
@@ -1,8 +1,10 @@
 import Construction from "@/components/General/Construction";
 import Stats from "@/components/Skills/Stats";
+import TopMenuContainer from "@/components/Home/TopMenuContainer";
 function Skills() {
   return (
-    <div className="h-screen w-screen bg-gray-950 flex flex-col items-center gap-2 md:flex-row">
+    <div className="h-screen w-screen bg-gray-950 flex flex-col gap-2">
+      <TopMenuContainer />
       <Stats />
     </div>
   );
